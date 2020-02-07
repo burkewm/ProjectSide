bae71a91895f6ca4caa8367c1f41756d06e422bb
William <burkewm@outlook.com>
2020-02-07 00:01:22 -0500

    Added in a main menu scene and some extras

---
+++ Assets/_Users/William/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Exit() {
+        Application.Quit();
+        Debug.Log("Appliaction Quit");
+    }
+
+    public void ShowSPMissions() {
+        //To Do
+    }
+
+    public void GoToMPLobby() {
+        //To Do
+    }
+    public void ShowCredits() {
+        //To Do
+    }
+}
